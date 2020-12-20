a796dd7a9d5fdf9a39d9901909d48ffa01a75b74
simonamihaylova <mihaylovamsimona@gmail.com>
2020-12-20 21:03:54 +0200

    Add word frequency to Bible

--- Dataset/Parsed/Bible/parse_bible.py
+++ Dataset/Parsed/Bible/parse_bible.py
@@ -1,4 +1,4 @@
-#commented lines are for up to 5 word instances
+#commented lines are for all word instances (but 1 per sentence)
 
 import nltk, csv
 from nltk.corpus import stopwords
@@ -9,8 +9,7 @@ in_file.close()
 
 stop_words = set(stopwords.words('english'))
 sentence_word = set()
-unique_words = set()
-#word_cnt = dict() 
+word_cnt = dict()
 
 sentences = nltk.sent_tokenize(text)
 
@@ -21,26 +20,30 @@ for sent in sentences:
     for word in tagged_words:
         if word[1] == "NOUN" and word[0].lower() not in stop_words:
 
- #           if word[0] in word_cnt and word_cnt.get(word[0]) >= 5:
-  #             continue;
+            if word[0].lower() in word_cnt and word_cnt.get(word[0].lower()) >= 5:
+                continue
 
-   #         if word[0] not in word_cnt:
-    #           word_cnt[word[0]] = 1
-            unique_words.add(word[0].lower())
-     #       elif word_cnt.get(word[0]) < 5 and (sent.replace('\n',' '), word[0]) not in nouns:
-      #          word_cnt[word[0]] = word_cnt.get(word[0])+1
+            if word[0].lower() not in word_cnt:
+                word_cnt[word[0].lower()] = 1
+            elif word_cnt.get(word[0].lower()) < 5 and (sent.replace('\n', ' '), word[0]) not in sentence_word:
+        #  else:
+                word_cnt[word[0].lower()] = word_cnt.get(word[0].lower()) + 1
 
             sentence_word.add((sent.replace('\n', ' '), word[0]))
 
+
 sentence_word = sorted(sentence_word, key=lambda x: x[1].lower())
 
-#with open('bible_single_word_up_to_5.tsv', 'wt') as out_file:
-
-with open('bible_single_word.tsv', 'wt') as out_file:
+with open('bible_single_word_up_to_5.tsv', 'wt') as out_file:
+#with open('bible_single_word.tsv', 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
     tsv_writer.writerows(sentence_word)
 
 out_file.close()
 
-#print(len(unique_words))
-#print(len(nouns))
+with open('bible_single_word_frequency_up_to_5.tsv', 'wt') as out_file:
+    tsv_writer = csv.writer(out_file, delimiter='\t')
+    tsv_writer.writerows(word_cnt.items())
+
+
+out_file.close()
